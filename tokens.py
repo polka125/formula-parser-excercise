2306680028f1cc4e9f6993ea117b295390674d49
Sergey Novozhilov <novozhilov1999@mail.ru>
2021-04-13 19:35:10 +0300

    Math formula tokens enum

---
+++ tokens.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class Token(Enum):
+    LEFT_BRACKET = r'('
+    RIGHT_BRACKET = r')'
+    ZERO = r'0'
+    ONE = r'1'
+    TWO = r'2'
+    THREE = r'3'
+    FOUR = r'4'
+    FIVE = r'5'
+    SIX = r'6'
+    SEVEN = r'7'
+    EIGHT = r'8'
+    NINE = r'9'
+    PLUS = r'+'
+    MINUS = r'-'
+    MULTIPLY = r'*'
+    DIVIDE = r'/'
+    SIN = r'sin'
+    COS = r'cos'
+    SQRT = r'sqrt'
+    POWER = r'^'
+    TAN = r'(tg|tan)'
+    LOG = r'(log|ln|lg)'
+
+
+
+
+
+
